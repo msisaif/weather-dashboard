ee40e61c9fdbd0d2950f5cf217f11bb5a128f0cc
msisaif <msi132043@gmail.com>
2024-02-14 22:29:12 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="text-center text-4xl underline">
+        Vite : React + Tailwind CSS
+      </div>
+    </div>
+  );
+}
+
+export default App;
